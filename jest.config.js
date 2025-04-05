fd185d7b4a63a70cde54ce2424d086624e1a135f
Edward McKnight <8123733+EM-Creations@users.noreply.github.com>
2025-04-05 21:16:28 +0100

    Junit reporting for Jest tests.

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   transformIgnorePatterns: [],
-  testPathIgnorePatterns: ["/node_modules/", "/tests/"]
+  testPathIgnorePatterns: ["/node_modules/", "/tests/"],
+  reporters: [ "default", "jest-junit" ]
 }
