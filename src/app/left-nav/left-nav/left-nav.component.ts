58b220ef0606e3e1890281dda7b8c33d5731f6ce
Edward McKnight <8123733+EM-Creations@users.noreply.github.com>
2025-07-28 20:56:37 +0100

    Login button for Auth0 authentication.

--- src/app/left-nav/left-nav/left-nav.component.ts
+++ src/app/left-nav/left-nav/left-nav.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'app-left-nav',
@@ -11,5 +12,10 @@ import {RouterLink, RouterLinkActive} from '@angular/router';
   styleUrl: './left-nav.component.scss'
 })
 export class LeftNavComponent {
-
+  protected domain = environment.auth0.domain;
+  protected clientId = environment.auth0.clientId;
+  protected audience = environment.auth0.audience;
+  protected redirectUri = environment.auth0.redirectUri;
+  protected scope = environment.auth0.scope;
+  protected state = environment.auth0.state;
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,13 @@
 export const environment = {
   production: true,
+  auth0: {
+    domain: "your_domain.auth0.com",
+    clientId: "your_client_id",
+    audience: "your_audience",
+    redirectUri: "http://localhost:4200",
+    scope: "openid profile email",
+    state: "xyz"
+  },
   graigFatha: {
     baseURL: "https://localhost:8080",
     tid: "999",
