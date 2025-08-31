aa7fa8f628128d4bff0c628b8f4d722b14d7fc17
Edward McKnight <8123733+EM-Creations@users.noreply.github.com>
2025-08-31 18:56:36 +0100

    Disabled the projects page while it isn't implemented.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import {Routes} from '@angular/router';
-import {ProjectsComponent} from './projects/projects/projects.component';
 import {HomeComponent} from './home/home/home.component';
 import {StatsComponent} from './stats/stats.component';
 import {authGuardFn} from '@auth0/auth0-angular';
@@ -8,6 +7,7 @@ import {ProfileComponent} from './profile/profile.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'profile', component: ProfileComponent, canActivate: [authGuardFn] },
-  { path: 'projects', component: ProjectsComponent, canActivate: [authGuardFn] },
+  // Re-enable the projects page once developed if it has a use.
+  // { path: 'projects', component: ProjectsComponent, canActivate: [authGuardFn] },
   { path: 'stats/:site', component: StatsComponent, canActivate: [authGuardFn] },
 ];
