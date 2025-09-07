95982e2795245bd4fbeaba1b37de9843fb973233
Edward McKnight <8123733+EM-Creations@users.noreply.github.com>
2025-09-07 12:59:13 +0100

    Fix staging redirectUri.

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -4,7 +4,7 @@ export const environment = {
     domain: "dev-energycoop.uk.auth0.com",
     clientId: "xN3rhmATd9PPs0I2bwzpdef2IUhaEQQo",
     audience: "http://localhost:8080/",
-    redirectUri: "https://energycoop-server.onrender.com",
+    redirectUri: "https://energycoop-ui.onrender.com",
     scope: "openid profile email",
     state: "xyz"
   },
